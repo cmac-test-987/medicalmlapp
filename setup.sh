a3fa054e36c1ad77c151b7d14d5df892daf97ec5
cmacidontcare <139860768+cmacidontcare@users.noreply.github.com>
2023-08-16 19:56:32 +0100

    Updated requirements.txt

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
+#!/bin/sh
+
 mkdir -p ~/.streamlit/
 
+# Fixing the missing double quote and adding a shebang line
 echo "\
 [general]\n\
-email = \"cfarr311y@gmail.com"\n\
+email = \"cfarr311y@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
+# Setting a default value for PORT
+PORT=${PORT:-8501}
 
 echo "\
 [server]\n\
