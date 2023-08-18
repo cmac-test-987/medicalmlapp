065625e76061ed7ec6df0adfae1756f4c98548f0
Cormac <cfarr311y@gmail.com>
2023-08-18 19:48:07 +0100

    Update App.py

--- App.py
+++ App.py
@@ -9,6 +9,7 @@ import sqlite3
 # Streamlit Configuration
 st.set_page_config(page_title="X-Ray Image Classifier", initial_sidebar_state="collapsed")
 
+
 # Styling and FontAwesome Integration
 st.markdown("""
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
